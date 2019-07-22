473df083cb52fdbf468817696efc98502c5ef9d8
arash <vahaabpour@gmail.com>
2019-07-22 15:48:27 -0700

    run settings

---
+++ stage_2__new_idea/scripts/train_and_test.sh
@@ -0,0 +1,1 @@
+~/anaconda3/bin/python train.py --gpu_ids 0,1 --net_idx 5 --no_flip --num_nets 8 --name cars.merged.context.5 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 64 --display_freq 600 --dataroot /home/shared/datasets/cars.merged.new
