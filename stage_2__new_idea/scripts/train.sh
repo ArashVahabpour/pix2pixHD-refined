d430e79d8c973b88c5581144168417cd5b339cdf
Arash Vahabpour <vahaabpour@gmail.com>
2019-07-22 18:14:20 -0700

    run scripts

---
+++ stage_2__new_idea/scripts/train.sh
@@ -0,0 +1,3 @@
+# train
+~/anaconda3/bin/python train.py --gpu_ids 0,1 --net_idx 0 --no_flip --num_nets 8 --name cars.merged.context.0 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 64 --display_freq 600 --dataroot /home/shared/datasets/cars.merged.new 
+
---
+++ stage_2__new_idea/scripts/val.sh
@@ -0,0 +1,2 @@
+# test
+--gpu_ids 0,1 --net_idx 0 --no_flip --num_nets 8 --name cars.merged.context.0 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
