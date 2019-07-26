f2fa497490ce8f66c62022d4ce389a75b1a44324
arash <vahaabpour@gmail.com>
2019-07-26 06:58:01 -0700

    updated val all as for loop

--- stage_2__kfold_and_refine/stage_2_1__kfold/scripts/val_all.sh
+++ stage_2__kfold_and_refine/stage_2_1__kfold/scripts/val_all.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # test
-{--gpu_ids 0,1 --net_idx 0 --no_flip --num_nets 8 --name cars.merged.context.0 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
---gpu_ids 2,3 --net_idx 1 --no_flip --num_nets 8 --name cars.merged.context.1 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
---gpu_ids 4,5 --net_idx 2 --no_flip --num_nets 8 --name cars.merged.context.2 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val} &
 
-{--gpu_ids 0,1 --net_idx 3 --no_flip --num_nets 8 --name cars.merged.context.3 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
---gpu_ids 2,3 --net_idx 4 --no_flip --num_nets 8 --name cars.merged.context.4 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
---gpu_ids 4,5 --net_idx 5 --no_flip --num_nets 8 --name cars.merged.context.2 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val} &
+cd $0 
+cd ..
 
-{--gpu_ids 0,1 --net_idx 6 --no_flip --num_nets 8 --name cars.merged.context.5 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
---gpu_ids 2,3 --net_idx 7 --no_flip --num_nets 8 --name cars.merged.context.6 --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val} &
+for NET_IDX in $(seq 0 7)
+do
+	~/anaconda3/bin/python test.py --gpu_ids 0 --net_idx $NET_IDX --no_flip --num_nets 8 --name cars.merged.context.$NET_IDX --label_nc 0 --no_instance --loadSize 256 --input_nc 3 --output_nc 1 --batchSize 1 --dataroot /home/shared/datasets/cars.merged.new --phase val
+done
